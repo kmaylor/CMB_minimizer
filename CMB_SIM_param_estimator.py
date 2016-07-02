c479a2befe29d452fc083c5e6b9d52e56f8a3b26
kmaylor <kmaylor52@gmail.com>
2016-07-02 15:59:42 -0700

    changed sims 100 to 400

--- CMB_SIM_param_estimator.py
+++ CMB_SIM_param_estimator.py
@@ -62,7 +62,7 @@ class like():
         self.param_names = ['theta','ombh2','ommh2','clamp','ns','tau','Asz','Aps','Acib']
 
         if not use_data: 
-            self.spectra = loadtxt(sim_path+'sims100/bandpower_sim_'+sim_num+'.txt')[:,-1]
+            self.spectra = loadtxt(sim_path+'sims400/bandpower_sim_'+sim_num+'.txt')[:,-1]
         else:
             self.spectra = loadtxt(sim_path+'/bandpower.txt')[:,-1]
             
